f834b27501126452e7b2ad4c715b9caf72104e2b
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-01-17 03:27:26 -0500

    some player sprites

--- Objects/Player/Animations.asm
+++ Objects/Player/Animations.asm
@@ -4,6 +4,8 @@
 SonicAniData:
 		dc.w SonicAni_Blank-SonicAniData
 		dc.w SonicANi_Blank-SonicAniData
+		dc.w SonicANi_Flap-SonicAniData
+		dc.w SonicANi_Flap-SonicAniData
 		dc.w SonicANi_Blank-SonicAniData
 		dc.w SonicANi_Blank-SonicAniData
 		dc.w SonicANi_Blank-SonicAniData
@@ -30,11 +32,11 @@ SonicAniData:
 		dc.w SonicANi_Blank-SonicAniData
 		dc.w SonicANi_Blank-SonicAniData
 		dc.w SonicANi_Blank-SonicAniData
-		dc.w SonicANi_Blank-SonicAniData
-		dc.w SonicANi_Blank-SonicAniData
-		dc.w SonicANi_Blank-SonicAniData
+		dc.w SonicANi_FlapB-SonicAniData
 SonicAni_Blank:		dc.b 1, 1, $FD, 0
 		even
+SonicANi_Flap:		dc.b	3,	$FD,	$1E
+SonicANi_FlapB:		dc.b	3,	1,	2,	3,	2,	1, $FD,	0
 sonicani_sprint:
 sonicani_run:
 sonicani_walk:
